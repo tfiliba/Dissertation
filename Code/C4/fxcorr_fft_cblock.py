667506925f07ff2b10e277009b427896c21b35f7
Terry Filiba <tfiliba@gmail.com>
2013-08-06 22:11:00 -0700

    First complete draft of C4

--- Code/C4/fxcorr_fft_cblock.py
+++ Code/C4/fxcorr_fft_cblock.py
@@ -1,10 +1,9 @@
-performance_model = CBlock.getADCModel(self.platforms, bandwidth, input_bitwidth)
+performance_model = CBlock.getFFTModel(self.platforms, bandwidth, input_bitwidth)
 data_source = 'PFB'
 source_connection = 0 #0 indicates a one-to-one connection
 source_bandwidth = 0.4*8
 data_sink = 'XEng'
-sink_connection_type = 1 #0 indicates an all-to-all connection
+sink_connection_type = 1 #1 indicates an all-to-all connection
 sink_bandwidth = 0.4*8
-antennas = 16
-self.blocks['FFT'] = CBlock(performance_model,data_source,source_connection,source_bandwidth,\
-    data_sink,sink_connection_type,fft_out_bandwidth,antennas)
+antennas = 16   # indicates the number of blocks to create
+self.blocks['FFT'] = CBlock(performance_model,data_source,source_connection,source_bandwidth, data_sink,sink_connection_type,fft_out_bandwidth,antennas)
